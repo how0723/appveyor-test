a07ffdb0b98b7a754cc29c0f074693141003554b
yehao <how886@qq.com>
2019-12-18 18:32:29 +0800

    add test

---
+++ test1.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+// 测试用例
+TEST_CASE("test1", "[test1]")
+{
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("test2", "[test1]")
+{
+    REQUIRE(1 != 2);
+}
+
+TEST_CASE("test3", "[test1]")
+{
+    REQUIRE(1 < 2);
+}
