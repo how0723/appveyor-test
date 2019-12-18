a24a9a9d2f4fd4b910ae6b2427b335f94a9a4551
yehao <how886@qq.com>
2019-12-18 17:19:00 +0800

    add

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
-    int a=0;
-    std::cout<<"a="<<a<<std::endl;
+    int a = 0;
+    std::cout << "a=" << a << std::endl;
+    std::cout << "a+1=" << a + 1 << std::endl;
     return 0;
 }
